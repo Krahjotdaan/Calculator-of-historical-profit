635bf0af352bcc9432c383a19227dc4edd575878
Ostapenko <abc@mail.ru>
2023-06-01 22:52:14 +0300

    Chore: updated admin

--- App/admin.py
+++ App/admin.py
@@ -4,4 +4,7 @@ import App.models as models
 admin.site.register(models.Currency)
 admin.site.register(models.Share)
 admin.site.register(models.PreciousMetal)
+admin.site.register(models.CurrencyRate)
+admin.site.register(models.ShareRate)
+admin.site.register(models.PreciousMetalRate)
 admin.site.register(models.SharesBriefcase)
