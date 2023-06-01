e9bb5ca573d7585647a2fe8566e78ba2448562e7
Ostapenko <abc@mail.ru>
2023-06-01 12:38:16 +0300

    Feat: upgraded forms

--- App/models.py
+++ App/models.py
@@ -1,74 +1,140 @@
 from django.db import models
 
 class Currency(models.Model):
-    state = models.CharField(max_length=100)
-    code = models.CharField(max_length=3)
-    exchange_rate = models.FloatField(default=0)
-    profit = models.FloatField(default=0)
+    name = models.CharField(max_length=100)
+    literal_code = models.CharField(max_length=3)
+    number_code = models.IntegerField(default=0)
+    state = models.CharField(max_length=100)  
 
-    def add(state, code, exchange_rate, profit):
+    @staticmethod
+    def add(name, literal_code, number_code, state):
         Currency.objects.create(
-            state=state,
-            code=code,
-            exchange_rate=exchange_rate,
-            profit=profit
+            name=name,
+            literal_code=literal_code,
+            number_code=number_code,
+            state=state
         )
 
+    @staticmethod
     def get():
         return Currency.objects.all()
-    
-    def update(exchange_rate, profit):
-        exchange_rate=exchange_rate
-        profit=profit
 
+    @staticmethod
     def delete():
         Currency.objects.delete()
 
 
 class Share(models.Model):
     name = models.CharField(max_length=100)
-    cost = models.FloatField(default=0)
-    profit = models.FloatField(default=0)
 
-    def add(name, cost, profit):
-        Share.objects.create(
-            name=name,
-            cost=cost,
-            profit=profit
-        )
+    @staticmethod
+    def add(name):
+        Share.objects.create(name=name)
 
+    @staticmethod
     def get():
         return Share.objects.all()
-    
-    def update(cost, profit):
-        cost=cost
-        profit=profit
 
+    @staticmethod
     def delete():
         Share.objects.delete()
 
 
 class PreciousMetal(models.Model):
     name = models.CharField(max_length=100)
-    cost = models.FloatField(default=0)
+
+    @staticmethod
+    def add(name):
+        PreciousMetal.objects.create(name=name)
+
+    @staticmethod
+    def get():
+        return PreciousMetal.objects.all()
+
+    @staticmethod
+    def delete():
+        PreciousMetal.objects.delete()
+
+
+class CurrencyRate(models.Model):
+    currency = models.ForeignKey(to=Currency, on_delete=models.CASCADE)
+    rate = models.FloatField(default=0)
     profit = models.FloatField(default=0)
 
-    def add(name, cost, profit):
-        PreciousMetal.objects.create(
-            name=name,
-            cost=cost,
+    @staticmethod
+    def add(currency, rate, profit):
+        CurrencyRate.objects.create(
+            currency=currency,
+            rate=rate,
             profit=profit
         )
 
+    @staticmethod
     def get():
-        return PreciousMetal.objects.all()
+        return CurrencyRate.objects.all()
     
-    def update(cost, profit):
-        cost=cost
-        profit=profit
+    @staticmethod
+    def update(rate, profit):
+        CurrencyRate.rate = rate
+        CurrencyRate.profit = profit
 
+    @staticmethod
     def delete():
-        PreciousMetal.objects.delete()
+        CurrencyRate.objects.delete()
+
+
+class ShareRate(models.Model):
+    share = models.ForeignKey(to=Share, on_delete=models.CASCADE)
+    rate = models.FloatField(default=0)
+    profit = models.FloatField(default=0)
+
+    @staticmethod
+    def add(share, rate, profit):
+        ShareRate.objects.create(
+            share=share,
+            rate=rate,
+            profit=profit
+        )
+
+    @staticmethod
+    def get():
+        return ShareRate.objects.all()
+    
+    @staticmethod
+    def update(rate, profit):
+        ShareRate.rate = rate
+        ShareRate.profit = profit
+
+    @staticmethod
+    def delete():
+        ShareRate.objects.delete()
+
+
+class PreciousMetalRate(models.Model):
+    metal = models.ForeignKey(to=PreciousMetal, on_delete=models.CASCADE)
+    rate = models.FloatField(default=0)
+    profit = models.FloatField(default=0)
+
+    @staticmethod
+    def add(metal, rate, profit):
+        PreciousMetalRate.objects.create(
+            metal=metal,
+            rate=rate,
+            profit=profit
+        )
+
+    @staticmethod
+    def get():
+        return PreciousMetalRate.objects.all()
+    
+    @staticmethod
+    def update(rate, profit):
+        PreciousMetalRate.rate = rate
+        PreciousMetalRate.profit = profit
+
+    @staticmethod
+    def delete():
+        PreciousMetalRate.objects.delete()
 
 
 class SharesBriefcase(models.Model):
